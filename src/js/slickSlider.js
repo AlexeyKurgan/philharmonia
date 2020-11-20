4b46d4df5965646553b945d31e052275f3dafdee
Alexey Kurgan <alexkurgan19927@gmail.com>
2020-11-20 14:55:43 +0200

    added mobile section whats on and whit us

--- src/js/slickSlider.js
+++ src/js/slickSlider.js
@@ -9,7 +9,19 @@ $(document).ready(function(){
         infinite:true,
         centerPadding:'50px',
         prevArrow: '<button id="prev" type="button" class="btn prev-arrow"><i class="fas fa-arrow-left"></i></button>',
-        nextArrow: '<button id="next" type="button" class="btn next-arrow"><i class="fas fa-arrow-right"></i></button>'
+        nextArrow: '<button id="next" type="button" class="btn next-arrow"><i class="fas fa-arrow-right"></i></button>',
+
+        responsive:[
+            {
+              breakpoint: 768,
+              settings: {
+                fade:false,
+                touchMove:true,
+                autoplaySpeed:10500,
+             }
+            }]
+        
     });
+ 
 });
 
